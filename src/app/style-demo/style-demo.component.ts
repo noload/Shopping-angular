b47e1925c365e15588ed707ead094d785a439844
vaibhav kamble <vaibhavkamble023@gmail.com>
2024-11-19 01:54:51 +0530

    ng style demo

--- src/app/style-demo/style-demo.component.ts
+++ src/app/style-demo/style-demo.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './style-demo.component.css'
 })
 export class StyleDemoComponent {
+  public bgStyle:string='red';
+  public textStyle:string='white';
+  public padding:string='0';
+  public fontSize:string='0';
 
+  public styleObj:{
+    'background-color':string,
+    'color':string,
+    'padding':string,
+    'font-size':string
+  }={
+    'background-color':'red',
+    'color':'white',
+    'padding':this.padding+"px",
+    'font-size':this.fontSize+"px"
+  }
+
+  applyStyle(){
+
+    this.styleObj={
+      'background-color':this.bgStyle,
+      'color':this.textStyle,
+      'font-size':this.fontSize+"px",
+      'padding':this.padding+"px"
+    }
+  }
 }
